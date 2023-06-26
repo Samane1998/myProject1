74eb9011c6f8753bbae75d5c2985192ba0ce3a1b
samane ghazanfari <samanegha12@gmail.com>
2023-06-27 02:31:22 +0430

    add validation for phone number

---
+++ login/login.js
@@ -0,0 +1,23 @@
+function validation() {
+    let uservalue = document.getElementById('phone').value;
+    // مقدار ورودی تگ اینپوت شماره موبایل را در یک متغیر ریختیم
+    let passvalue = document.getElementById('pass').value;
+    // مقدار ورودی تگ اینپوت رمز عبور را در یک متغیر ریختیم
+    let isUserValid = uservalue.length == 11;
+    // تعیین طول رشته ی فیلد موبایل
+    let numValid = false;
+      // ایجاد یک متغیر با مقدار بولین برای استفاده در شرط ها
+      for (let i = 0 ; i < 11 ; i++) {
+        // استفاده از حلقه برای پیمایش بر روی کارکتر های ورودی برای اعتبارسنجی شماره موبایل
+        if (uservalue[i] <= 9 && uservalue[i] >= 0) numValid = true;
+        // ایجاد شرط برای درست وارد شدن اعداد
+        // مقدار بولین را در این حالت درست قرارمیدهیم 
+      }
+      if (isUserValid) {
+        // اگر کاربر شماره خود را درست وارد کرده باشد پیغام میاد که فیلد بعدی را پر کند
+          document.getElementById('usererror').innerHTML = "لطفا رمز عبور خود را وارد کنید";
+      } else {
+        // و اگر شماره نادرست باشد پیغام خطا میاد که شماره خود را درست وارد کند 
+          document.getElementById('usererror').innerHTML = "شماره خود را ویرایش و درست وارد کنید ";
+      };
+  };
